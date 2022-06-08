92958ed77ff675250f1470643e68a65be279e5dc
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2022-06-08 13:39:58 -0700

    fixed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,6 +5,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
@@ -12,12 +13,12 @@ class SearchBar extends React.Component {
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
+            <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
               onChange={(e) => this.setState({ term: e.target.value })}
             />
-            <label>Image Search</label>
           </div>
         </form>
       </div>
