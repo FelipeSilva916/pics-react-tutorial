0a5d005f42f616686f7abcf4f600fde1d8b48198
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2022-06-09 08:06:44 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID ex7cdgC1um8X05A72_yWFh-fJpESLUhBKGx1nLOXZFg"
       }
     });
+    console.log(response.data.results);
   }
   render() {
     return (
